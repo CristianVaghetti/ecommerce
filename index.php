bc68fd203c793952c2a32773d7a855161649acc8
CristianVaghetti <vaghetticristian@gmail.com>
2021-05-18 09:06:57 -0300

    criado classe Page para começar a se formar a pagina

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
-	$results = $sql -> select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
-
+    $page = new Page(); // abre a pagina -> adiciona o header e o body
+	$page -> setTpl("index"); // carrega o conteudo dentro do body
+	// fecha o header e o body e coloca o footer
 });
 
 $app->run();
