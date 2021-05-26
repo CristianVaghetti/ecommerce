e67858ae3ec8d4245521f58d2e27c91aab30f413
CristianVaghetti <vaghetticristian@gmail.com>
2021-05-26 11:10:18 -0300

    adicionado o page admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -11,6 +12,12 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     $page = new Page(); // abre a pagina -> adiciona o header e o body
+	$page -> setTpl("index"); // carrega o conteudo dentro do body
+	// fecha o header e o body e coloca o footer
+});
+
+$app->get('/admin', function() {
+    $page = new PageAdmin(); // abre a pagina -> adiciona o header e o body
 	$page -> setTpl("index"); // carrega o conteudo dentro do body
 	// fecha o header e o body e coloca o footer
 });
