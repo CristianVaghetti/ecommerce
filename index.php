52d76ea6e9d3cedb29c4893d52ee2ee999881898
CristianVaghetti <vaghetticristian@gmail.com>
2021-05-26 12:35:04 -0300

    lancei com erro

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -20,6 +21,18 @@ $app->get('/admin', function() {
     $page = new PageAdmin(); // abre a pagina -> adiciona o header e o body
 	$page -> setTpl("index"); // carrega o conteudo dentro do body
 	// fecha o header e o body e coloca o footer
+});
+
+$app->get('/admin/login', function() {
+    $page = new PageAdmin(["header" => false, "footer" => false]); // abre a pagina -> adiciona o header e o body
+	$page -> setTpl("login"); // carrega o conteudo dentro do body
+	// fecha o header e o body e coloca o footer
+});
+
+$app->post('/admin/login', function() {
+	User::login($_POST["login"], $_POST["password"]);
+	header("Location: /admin");
+	exit;
 });
 
 $app->run();
